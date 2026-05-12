778aa45fc6b3684a3a3d80fe7784340a99e532c1
Brooklynne Audette <brooklynneaudette@macbookpro.lan>
2026-05-12 15:09:08 -0500

    use new components in main index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,79 @@
-import useBetterTheme from "@/hooks/useBetterTheme";
-import { Text, View, TouchableOpacity } from "react-native";
+import { createHomeStyles } from "@/assets/images/styles/home.styles";
 
+import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import TodoInput from "@/components/TodoInput";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import useBetterTheme from "@/hooks/useBetterTheme";
+import { useQuery } from "convex/react";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import {
+  FlatList,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type Todo = Doc<"todos">;
 
 export default function Index() {
-  const {toggleDarkMode} = useBetterTheme();
+  const { betterColors } = useBetterTheme();
+
+  const homeStyles = createHomeStyles(betterColors);
+
+  const todos = useQuery(api.todos.getTodos); //gets all todos
+  //we are using a flatlist for these because it renders better with larger lists,
+  //  but for a small list like 15, we could also just do todos.map(todo => <Text>{todo.text}</Text>)
+  const isLoading = todos === undefined; //convex returns undefined while loading, then returns the data once it's ready.
+  if (isLoading) return <LoadingSpinner />;
+
+  const renderTodoItem = ({ item }: { item: Todo }) => {
+    return (
+      <View style={homeStyles.todoItemWrapper}>
+        <LinearGradient
+          colors={betterColors.gradients.surface}
+          style={homeStyles.todoItem}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+        >
+          <TouchableOpacity
+            style={homeStyles.checkbox}
+            activeOpacity={0.7}
+            onPress={() => {}}
+          ></TouchableOpacity>
+          <LinearGradient
+            colors={
+              item.isCompleted
+                ? betterColors.gradients.success
+                : betterColors.gradients.muted
+            }
+          ></LinearGradient>
+        </LinearGradient>
+      </View>
+    );
+  };
   return (
-    <View
-    style={{justifyContent: "center", alignItems: "center", flex: 1}}
+    <LinearGradient
+      colors={betterColors.gradients.background}
+      style={homeStyles.container}
     >
-    <Text >Text shows up in the middle here. In a Column flex by default.</Text>
-  <TouchableOpacity onPress={toggleDarkMode}>
-    <Text>toggle the dark mode</Text>
-  </TouchableOpacity>
-    </View>
-  );  
+      <StatusBar barStyle={betterColors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.container}>
+        <Header />
+        <TodoInput />
+
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={(item) => item._id}
+          style={homeStyles.todoList}
+          contentContainerStyle={homeStyles.todoListContent}
+        />
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
-
-
-
-
- 
